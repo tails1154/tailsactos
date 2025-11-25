ba3c90b34ca15b3640f111df7dbe6e986ecbf493
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-11-25 12:34:36 -0600

    i have committed to make this commit

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./configure.sh && cd output-MinGW-i386/ && rm disk.qcow2 ; qemu-img create -f qcow2 disk.qcow2 10G && ninja && ninja bootcd && qemu-system-i386 -cdrom bootcd.iso -hda disk.qcow2
+./configure.sh -DENABLE_ROSAPPS=1 && cd output-MinGW-i386/ && rm disk.qcow2 ; qemu-img create -f qcow2 disk.qcow2 10G && ninja && ninja bootcd && qemu-system-i386 -cdrom bootcd.iso -hda disk.qcow2
